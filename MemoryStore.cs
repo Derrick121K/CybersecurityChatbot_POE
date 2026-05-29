8aa22016fa5dfd362be09fad347e7091e51541eb
derkdev976-web <derkdev976@gmail.com>
2026-05-29 14:11:24 +0200

    feat: Add memory store for user name and favorite topic

--- MemoryStore.cs
+++ MemoryStore.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace CybersecurityChatbot
+namespace CybersecurityChatbot;
+
+public class MemoryStore
 {
-    internal class MemoryStore
+    public string UserName { get; set; } = "";
+    public string FavoriteTopic { get; set; } = "";
+    private readonly Dictionary<string, string> _memories = new();
+
+    public void Store(string key, string value)
     {
+        if (_memories.ContainsKey(key))
+            _memories[key] = value;
+        else
+            _memories.Add(key, value);
+    }
+
+    public string? Recall(string key)
+    {
+        return _memories.GetValueOrDefault(key);
+    }
+
+    public string GetPersonalizedOpener()
+    {
+        if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(FavoriteTopic))
+        {
+            return $"As someone interested in {FavoriteTopic}, {UserName}, ";
+        }
+        else if (!string.IsNullOrEmpty(UserName))
+        {
+            return $"{UserName}, ";
+        }
+        else if (!string.IsNullOrEmpty(FavoriteTopic))
+        {
+            return $"Since you're interested in {FavoriteTopic}, ";
+        }
+        return "";
     }
 }
